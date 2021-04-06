3ce085c47354fc06999c68878c7e6bfd89778426
Gimmmick <vlada310999@mail.ru>
2021-04-06 14:09:46 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+The program calculates and displays the change in price
+for a country club membership over the period.
+
+@author Vlada Foulke
+@version 11/01/2020
+*******************************************************************************/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double price = 2500;
+    double percent = 0.04;
+    int period_years = 6; 
+    
+    cout << "Original fee for the country club membership is $" << price << endl;
+    cout << "The price will be increased by 4% each year for the next "
+         << period_years << " years\n\n";
+    
+    for (int number = 1; number <= period_years; number++) {
+        price += price * percent;
+        cout << setw(4) << number << " year" << setw(4) << '$' << price << endl;
+    }
+    
+    return 0;
+}
